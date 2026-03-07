65c1d49daf0170b8a29e55e35a05479f6c744cea
Ahmed Omeiza <omeizaahmed81@gmail.com>
2026-03-07 23:10:39 +0100

    Add getUserSubscriptions controller and update routes

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -11,3 +11,21 @@ export const createSubscription = async (req, res, next) => {
     next(error);
   }
 }
+
+
+export const getUserSubscriptions = async (req, res, next) => {
+  try {
+    // check if the user is the same as the one in the token
+    if(req.user._id.toString() !== req.params.id){
+      const error = new Error('You are not the owner of this account');
+      error.status = 401;
+      throw error;
+    }
+    const subscriptions = await Subscription.find({ user: req.params.id });
+    res.status(200).json({success: true, data: subscriptions});
+  } catch (error) {
+    next(error);
+  }
+}
+
+// 220
